b2d1172f6ee25e40b5657bdf5314fc72457fba3b
M-Factory <mario.felipe.tf@gmail.com>
2026-03-23 14:36:21 +0900

    Update esptimecast.js

--- esptimecast.js
+++ esptimecast.js
@@ -80,7 +80,7 @@ const READ_REG_PACKET = slipEncode([0x00, 0x0a, 0x04, 0x00, 0x00, 0x00, 0x00, 0x
 
 const manifest = {
     name: "ESPTimeCast",
-    version: "1.4.2",
+    version: "1.5.0",
     builds: []
 };
 
